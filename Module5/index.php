711326921dd2ba9c0235cb29faa10fcfd53a6e44
Joseph Wu <josephwu0507@gmail.com>
2024-08-20 12:07:19 -0400

    add Module5 - first php

---
+++ Module5/index.php
@@ -0,0 +1,22 @@
+<title>Joseph Wu PHP</title>
+<h1>Joseph Wu PHP</h1>
+
+<?php
+echo 'The SHA256 has of "Joseph Wu" is ';
+print hash('sha256', 'Joseph Wu');
+?>
+
+<pre>
+ASCII ART:
+    **************
+    **************
+          ****
+          ****
+          ****
+          ****
+      **  ***
+      *****
+</pre>
+
+<a href="check.php" style="display: block;">Click here to check the error setting</a>
+<a href="fail.php">Click here to cause a traceback</a>
